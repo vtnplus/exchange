1927b4782032cf3a791c535eae85ebff553fd2e2
MacQ <macq@MacQs-MacBook.local>
2018-03-27 18:08:02 +0700

    core

---
+++ resource/language/vi/globals.php
@@ -0,0 +1,12 @@
+<?php
+return [
+	"login_title" => "Đăng nhập",
+	"register"	=>	"Đăng ký",
+	"btn_login" => "Thực hiện đăng nhập",
+	"forget"	=>	"Quên mật khẩu",
+	"email"		=>	"Địa chỉ email",
+	"password"	=>	"Mật khẩu",
+	"or_with"	=>	"Hoặc đăng nhập qua",
+	"register_title"	=>	"Đăng ký tài khoản",
+	"btn_register"	=>	"Gởi đăng ký",
+];
