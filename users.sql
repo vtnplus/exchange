2c858205f3492fb309c008e80cf89260084f2449
Anh Khoa <anhkhoa@Anhs-iMac.local>
2018-03-30 07:36:04 +0700

    Up

--- users.sql
+++ users.sql
@@ -1,23 +1,17 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.3
--- https://www.phpmyadmin.net/
+-- version 4.3.10
+-- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Mar 30, 2018 at 12:57 AM
--- Server version: 5.6.35
--- PHP Version: 7.1.8
+-- Generation Time: Mar 30, 2018 at 02:35 AM
+-- Server version: 5.5.42
+-- PHP Version: 5.6.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Database: `users`
+-- Database: `exchange_ci`
 --
 
 -- --------------------------------------------------------
@@ -34,6 +28,42 @@ CREATE TABLE `account` (
   `account_login` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `account_attach` int(2) NOT NULL,
   `account_lock` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `admin_login` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `admin_password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `admin_status` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `admin_lever` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `admin_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `account_id` int(11) NOT NULL,
+  `admin_lastlogin` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `admin_attach` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `api_service`
+--
+
+CREATE TABLE `api_service` (
+  `api_id` int(11) NOT NULL,
+  `api_username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `api_password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `api_lastlogin` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `fee_balancer_id` int(11) NOT NULL,
+  `api_status` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `api_domain` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `api_attach` int(11) NOT NULL,
+  `api_hmac` varchar(255) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -138,6 +168,18 @@ ALTER TABLE `account`
   ADD PRIMARY KEY (`account_id`);
 
 --
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `api_service`
+--
+ALTER TABLE `api_service`
+  ADD PRIMARY KEY (`api_id`);
+
+--
 -- Indexes for table `balancer`
 --
 ALTER TABLE `balancer`
@@ -177,6 +219,16 @@ ALTER TABLE `validate`
 ALTER TABLE `account`
   MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `api_service`
+--
+ALTER TABLE `api_service`
+  MODIFY `api_id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `balancer`
 --
 ALTER TABLE `balancer`
@@ -201,6 +253,3 @@ ALTER TABLE `trade_service`
 --
 ALTER TABLE `validate`
   MODIFY `validate_id` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
